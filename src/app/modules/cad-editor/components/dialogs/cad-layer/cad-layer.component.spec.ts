5de49f1ac22a416663ee816d920992685bc42b58
Lucilor <lucilor@me.com>
2023-11-21 17:24:48 +0800

    🔧 build: yarn 4

--- src/app/modules/cad-editor/components/dialogs/cad-layer/cad-layer.component.spec.ts
+++ src/app/modules/cad-editor/components/dialogs/cad-layer/cad-layer.component.spec.ts
@@ -2,10 +2,10 @@ import {ComponentFixture, TestBed} from "@angular/core/testing";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {MatIconModule} from "@angular/material/icon";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {CadLayer} from "@lucilor/cad-viewer";
 import {InputModule} from "@modules/input/input.module";
 import {MessageModule} from "@modules/message/message.module";
 import {NgScrollbarModule} from "ngx-scrollbar";
-import {CadLayer} from "packages/cad-viewer/lib";
 import {CadLayerComponent, CadLayerInput} from "./cad-layer.component";
 
 describe("CadLayerComponent", () => {
