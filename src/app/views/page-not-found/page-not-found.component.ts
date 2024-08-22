a241c64f5b6673196f1effccca400acddf6ac9ed
lucilor <lucilor@me.com>
2024-08-22 09:59:52 +0800

    fix(page-not-found): call setProject when page not found

--- src/app/views/page-not-found/page-not-found.component.ts
+++ src/app/views/page-not-found/page-not-found.component.ts
@@ -1,26 +1,42 @@
-import {Component} from "@angular/core";
+import {ChangeDetectionStrategy, Component, inject, OnInit, signal} from "@angular/core";
 import {MatButtonModule} from "@angular/material/button";
 import {ActivatedRoute, Router} from "@angular/router";
 import {PathResolveData} from "@app/routing/path-resolver";
+import {AppStatusService} from "@app/services/app-status.service";
 
 @Component({
   selector: "app-page-not-found",
   templateUrl: "./page-not-found.component.html",
   styleUrls: ["./page-not-found.component.scss"],
   standalone: true,
-  imports: [MatButtonModule]
+  imports: [MatButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PageNotFoundComponent {
-  data: PathResolveData;
+export class PageNotFoundComponent implements OnInit {
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private status = inject(AppStatusService);
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router
-  ) {
-    this.data = this.route.snapshot.data.redirect || {path: "", queryParams: {}};
+  data = signal<PathResolveData | null>(null);
+
+  constructor() {
+    this.route.data.subscribe((data) => {
+      if (data.redirect) {
+        this.data.set(data.redirect);
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.status.setProject(this.route.snapshot.queryParams);
   }
 
   redirect() {
-    this.router.navigate([this.data.path], {queryParams: this.data.queryParams});
+    const data = this.data();
+    if (!data) {
+      return;
+    }
+    const {path, queryParams} = data;
+    this.router.navigate([path], {queryParams});
   }
 }
