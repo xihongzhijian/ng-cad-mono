99c2ff6702064ac176c3359d5a3581127c976385
lucilor <lucilor@me.com>
2025-03-11 11:03:29 +0800

    feat(cad): 添加刷新属性按钮

--- src/app/modules/cad-editor/components/cad-editor/cad-editor.component.ts
+++ src/app/modules/cad-editor/components/cad-editor/cad-editor.component.ts
@@ -593,6 +593,13 @@ export class CadEditorComponent extends Subscribed() implements AfterViewInit, O
       this.hideMenuSearch();
     }
   }
+
+  showTabs = signal(true);
+  async refreshTabs() {
+    this.showTabs.set(false);
+    await timeout(0);
+    this.showTabs.set(true);
+  }
 }
 
 type Dragkey = keyof Pick<AppConfig, "leftMenuWidth" | "rightMenuWidth">;
